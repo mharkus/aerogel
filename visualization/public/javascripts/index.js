d891ca54ae256f5128e7ac560f02c7df83be98f0
Marc Tan <marc.lester.tan@sap.com>
2013-10-19 17:16:02 +0800

    fixed small layout issue

--- visualization/public/javascripts/index.js
+++ visualization/public/javascripts/index.js
@@ -2,10 +2,6 @@ var socket;
                 
             var gauges = [];
 
-                    // revolutions per second
-            var angularSpeed = 0.2; 
-            var lastTime = 0;
-            var camera, scene, plane, renderer;
 
             
             function createGauge(name, label, min, max, size)
@@ -86,117 +82,6 @@ var socket;
                 });
               }
 
-           function draw3D(){
-                // renderer
-              renderer = new THREE.WebGLRenderer();
-              renderer.setSize(400, 350);
-              document.getElementById('stabilizer3D').appendChild(renderer.domElement);
-         
-              // camera
-              camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
-              camera.position.y = 150;
-              camera.position.z = 500;
-              //camera.rotation.x = 45 * (Math.PI / 180);
-         
-              // scene
-              scene = new THREE.Scene();
-                
-             var geometry = new THREE.CubeGeometry( 200, 200, 10 );
-             var colors = ["0xff0000", "0xff0000", "0x00ffe4", "0x00ffe4", "0xfcff00", "0xfcff00", "0x0006ff", "0x0006ff", "0x46a145", "0x46a145", "0xff5a00", "0xff5a00"];
-
-                for ( var i = 0; i < geometry.faces.length; i ++ ) {
-                    geometry.faces[ i ].color.setHex(colors[i]);
-                }
-
-               var material = new THREE.MeshBasicMaterial( { vertexColors: THREE.FaceColors, overdraw: 0.5 } );
-
-              cube = new THREE.Mesh( geometry, material );
-              cube.position.y = 150;
-              cube.position.x = -150;
-
-
-
-              scene.add(cube);
-
-              renderer.render( scene, camera ); 
-              //animate();
-           }   
-
-           function plot(){
-                nv.addGraph(function() {
-                  chart = nv.models.lineChart()
-                  .options({
-                    margin: {left: 100, bottom: 100},
-                    x: function(d,i) { return i},
-                    showXAxis: true,
-                    showYAxis: true,
-                    transitionDuration: 250
-                  });
-
-                // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-                chart.xAxis
-                .axisLabel("Time (s)")
-                .tickFormat(d3.format(',.1f'));
-
-              chart.yAxis
-                .axisLabel('Voltage (v)')
-                .tickFormat(d3.format(',.2f'))
-                ;
-
-              d3.select('#plot svg')
-                .datum(sinAndCos())
-                .call(chart);
-
-              //TODO: Figure out a good way to do this automatically
-              nv.utils.windowResize(chart.update);
-              //nv.utils.windowResize(function() { d3.select('#chart1 svg').call(chart) });
-
-              chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
-
-              return chart;
-            }); 
-          }
-           
-
-        function sinAndCos() {
-          var sin = [],
-            cos = [],
-            rand = [],
-            rand2 = []
-            ;
-
-          for (var i = 0; i < 100; i++) {
-            sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) }); //the nulls are to show how defined works
-            cos.push({x: i, y: .5 * Math.cos(i/10)});
-            rand.push({x:i, y: Math.random() / 10});
-            rand2.push({x: i, y: Math.cos(i/10) + Math.random() / 10 })
-          }
-
-          return [
-            {
-              area: true,
-              values: sin,
-              key: "Sine Wave",
-              color: "#ff7f0e"
-            },
-            {
-              values: cos,
-              key: "Cosine Wave",
-              color: "#2ca02c"
-            },
-            {
-              values: rand,
-              key: "Random Points",
-              color: "#2222ff"
-            }
-            ,
-            {
-              values: rand2,
-              key: "Random Cosine",
-              color: "#667711"
-            }
-          ];
-        }
 
 function drawFlightPanel(){
   var canvas = document.getElementById('footerbg');
